0467f4829e33cd33600a0d0d9f8bdbeb54241749
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2022-05-15 10:15:15 +0200

    WIP :: Update Model Structure

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,10 +1,12 @@
-const User = function (row){
-    this.id = row.id;
-    this.name = row.name;
-    this.email = row.email;
-    this.password = row.password;
-    this.createdAt = row.created_at;
-    this.updatedAt = row.updated_at;
+class User {
+    constructor(row) {
+        this.id = row.id;
+        this.name = row.name;
+        this.email = row.email;
+        this.password = row.password;
+        this.createdAt = row.created_at;
+        this.updatedAt = row.updated_at;
+    }
 }
 
 module.exports = User;
